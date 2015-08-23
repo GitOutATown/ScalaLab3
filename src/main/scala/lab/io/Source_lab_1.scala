77ac82db503f2fad2f4fadc8caa50e7c5647200d
GitOutATown <aqumegeemail@gmail.com>
2015-08-23 15:56:26 -0700

    backup

--- src/main/scala/lab/io/Source_lab_1.scala
+++ src/main/scala/lab/io/Source_lab_1.scala
@@ -1,13 +1,14 @@
 package lab.io
 
+import common.Path.root_path
 import scala.io.Source
 
 object Source_lab_1 extends App {
   
-	val path = "/Users/hieronymus/Development/Workspace_BAK/ScalaLab3/src/main/data/"
+    val path = "src/main/data/"
 	val docName = "Vibrio_cholerae.txt"
 	
-	val source = Source.fromFile(path + docName)
+	val source = Source.fromFile(root_path + path + docName)
 	val text = source.getLines.mkString
 	source.close // should be in finally
 	
