2ca13a996e1bae1246a3eb296aefad14b60aca2c
GitOutATown <aqumegeemail@gmail.com>
2015-08-23 22:27:04 -0700

    backup

--- src/main/scala/lab/nlp/vectorspacemodel/tfidf/journal/JournalParser_3.scala
+++ src/main/scala/lab/nlp/vectorspacemodel/tfidf/journal/JournalParser_3.scala
@@ -1,40 +1,43 @@
 package lab.nlp.vectorspacemodel.tfidf.journal
 
 object JournalParser_3 extends App {
-
-  val text = List ("1/2/03", "Abc def.", "Ghi klm.", "Nop qrs.", "11/22/04", "Tuv wxy.", "Zhi ozh.", "14/30/06", "Foo bar.", "Too much.")
-  val datePattern = """[0-9]{1,2}/[0-9]{1,2}/[0-9]{1,4}""".r
-  
-  def isDate(line: String): Boolean = {
-    datePattern.findPrefixMatchOf(line) match {
-      case Some(_) => true
-      case None => false
-    }
-  }
-  
-  def process(text: List[String]): List[List[String]] = {
     
-    def inter(interText: List[String], acc1: List[String], acc2: List[List[String]]): List[List[String]] = {
-      interText match {
-        case Nil => acc2 :+ acc1
-        case line :: tail => {
-          if(isDate(line)) {
-            val newAcc2 = if(acc1.isEmpty) acc2 else acc2 :+ acc1
-            inter(tail, List(line), newAcc2)
-          }
-          else {
-            val newAcc1 = acc1 :+ line
-            inter(tail, newAcc1, acc2)
-          }
+    case class JournalEntry(date: String, text: List[String])
+
+    val text = List ("1/2/03", "Abc def.", "Ghi klm.", "Nop qrs.", "11/22/04", "Tuv wxy.", "Zhi ozh.", "14/30/06", "Foo bar.", "Too much.")
+    val datePattern = """[0-9]{1,2}/[0-9]{1,2}/[0-9]{1,4}""".r
+    
+    def isDate(line: String): Boolean = {
+        datePattern.findPrefixMatchOf(line) match {
+            case Some(_) => true
+            case None => false
         }
-      }
-    } // End inter
+    }
     
-    inter(text, List[String](), List[List[String]]())
-  } // End process
+    def process(text: List[String]): List[JournalEntry] = {
+        
+        def inter(interText: List[String], journal: List[JournalEntry], entry: JournalEntry = null): List[JournalEntry] = {
+            interText match {
+                case Nil => journal :+ entry
+                case line :: tail => {
+                    if(isDate(line)) {
+                        val date = line
+                        val iterJournal = if(entry == null) journal else journal :+ entry
+                        inter(tail, iterJournal, JournalEntry(date, List[String]()))
+                    }
+                    else {
+                        val iterEntry = JournalEntry(entry.date, entry.text :+ line)
+                        inter(tail, journal, iterEntry)
+                    }
+                }
+            }
+        } // End inter
+        
+        inter(text, List[JournalEntry]())
+    } // End process
 
-  val result = process(text)
-  
-  println(result)
+    val result = process(text)
+    
+    println(result)
 
 }
