8ab425ae5f9bc8c91454a6a6490edbec885a49cb
GitOutATown <aqumegeemail@gmail.com>
2015-08-24 00:51:25 -0700

    backup

---
+++ src/main/scala/lab/nlp/vectorspacemodel/tfidf/journal/TfidfClient_journal.scala
@@ -0,0 +1,43 @@
+package lab.nlp.vectorspacemodel.tfidf.journal
+
+import scala.io.Source
+import lab.nlp.vectorspacemodel.tfidf.journal.JournalParser_4._
+import lab.nlp.vectorspacemodel.tfidf.journal.TfIdfService_6._
+import common.Path._
+
+object TfidfClient_journal extends App {
+
+    val resource_path = "src/main/scala/resources/"
+    val path = root_path + resource_path
+    val docName = "rw journal - sv 3.txt"
+    
+    val doc = Source.fromFile(journalPath + docName)
+    val text = doc.getLines.toList
+    doc.close // should be in finally
+    
+    val stopwords = Source.fromFile(path + "stopwords2.txt").getLines.toList
+        .filter(w => w != "")
+    
+    val journalEntries = process(text)
+    
+    println("result length: " + journalEntries.length)
+    journalEntries.take(2).foreach { println }
+    println("------------")
+    
+    val allTFIDFs = tfidf(journalEntries, stopwords)
+    
+    allTFIDFs foreach {
+        doc => {
+            println(doc._1)
+            println(doc._2.take(10))
+            println()
+        }
+    }
+    
+    
+
+    
+    
+    
+    
+}
