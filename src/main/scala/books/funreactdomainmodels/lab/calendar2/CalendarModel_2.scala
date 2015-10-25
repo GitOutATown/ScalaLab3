546b64f45ff404e48ec50c78cc4ccb9c3cac86fe
GitOutATown <aqumegeemail@gmail.com>
2015-10-25 13:55:47 -0700

    Added 2nd version of Calendar model

--- src/main/scala/books/funreactdomainmodels/lab/calendar2/CalendarModel_2.scala
+++ src/main/scala/books/funreactdomainmodels/lab/calendar2/CalendarModel_2.scala
@@ -1,4 +1,4 @@
-package books.funreactdomainmodels.lab.calendar
+package books.funreactdomainmodels.lab.calendar2
 
 import java.util.Date
 import scala.util.{Try, Success, Failure}
@@ -12,28 +12,59 @@ trait CalandarService {
     def getMonth(date: Date): Try[Month]
     def getYear(date: Date): Try[Year]
     def getHolidays(holidays: List[Holiday])
+    
+    def newCalendar(uId: User): Try[Calendar]
 }
 
 trait EventService {
     import CalendarEntities._
     
-    def newEvent: Try[Event]
+    def addEvent(calendar: Calendar, event: Event): Try[(Calendar)] = {
+        val newCalendar = calendar.copy(events = event :: calendar.events)
+        Success(newCalendar)
+    }
     
-    def deleteEvent(event: Event): Try[Event]
-    def findEvent(calendar: Calendar, name: String): Try[List[Event]]
+    def deleteEvent(calendar: Calendar, event: Event): Try[Calendar] = {
+        val newEvents = calendar.events.filter(e => e != event)
+        Success(calendar.copy(events = newEvents))
+    }
+    
+    def findEvent(calendar: Calendar, name: String): Try[List[Event]] = {
+        val foundEvents = calendar.events.filter(_.name == name)
+        Success(foundEvents)
+    }
+    
     def editEvent(event: Event): Try[Event]
+    
     def eventsBy(startTime: Date, endTime: Date): Try[List[Event]]
     
-    def addReminder(event: Event, reminder: Reminder): Try[Reminder]
-    def deleteReminder(event: Event, reminder: Reminder): Try[Reminder]
+    def addReminder(event: Event, date: Date): Try[Event] = {
+        val newReminders = Reminder(date) :: event.reminders
+        Success(event.copy(reminders = newReminders))
+    }
+    
+    def deleteReminder(event: Event, reminder: Reminder): Try[Event] = {
+        val newReminders = event.reminders.filter { 
+            r => r != reminder 
+        }
+        Success(event.copy(reminders = newReminders))
+    }
     
     def changeReminder(
         event: Event, oldReminder: Reminder, newDate: Date
     ): Try[Event] = {
         val newReminders = Reminder(newDate) :: event.reminders.filter { 
-            reminder => reminder != oldReminder 
+            r => r != oldReminder 
         }
         Success(event.copy(reminders = newReminders))
+    }
+    
+    def transferEvent(sourceCal: Calendar, targetCal: Calendar, event: Event)
+        : Try[(Calendar, Calendar)] = {
+        for {
+            sCal <- deleteEvent(sourceCal, event)
+            tCal <- addEvent(targetCal, event)
+        } yield (sCal, tCal)
     }
 }
     
@@ -66,10 +97,16 @@ object CalendarEntities {
         invites: List[Email]
     )
     
-    case class Calendar(user: User)
+    case class Calendar(
+        user: User, events: List[Event], holidays: List[Holiday]
+    )
+    
     case class Year(months: List[Month])
+    
     case class Month(days: List[Day])
+    
     case class Week(days: List[Day])
+    
     case class Day(date: Date)
     
     case class Location(name: String, address: Address, map: Map)
