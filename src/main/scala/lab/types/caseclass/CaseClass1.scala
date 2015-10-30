821aea1a5cabcd12569d12f8e98999f42974e3fb
GitOutATown <aqumegeemail@gmail.com>
2015-10-29 18:34:25 -0700

    Able to do variable case class copy as members of list where all are same trait.

--- src/main/scala/lab/types/caseclass/CaseClass1.scala
+++ src/main/scala/lab/types/caseclass/CaseClass1.scala
@@ -10,17 +10,46 @@ object CaseClass1 extends App {
     val t1: Term = Var("t1")
     val t2: Term = Fun("t2", t1)
     val t3: Term = App(t1, t2)
+    val t4: Term = Var("t4")
     
-    val list = List(t1, t2, t3)
+    val list1 = List(t1, t2, t3)
+    println("list1:")
+    list1 foreach println
     
+    val list2 = list1 map altTerm
+    println("\nlist2:")
+    list2 foreach println
+    
+    def altTerm(t: Term): Term = t match {
+        case t: Var => {
+            val varAltName = t.copy(name = t.name + "Alt")
+            //println("Got Var, new name is " + varAltName.name)
+            varAltName
+        }
+        case t: Fun => {
+            val funAltBody = t.copy(body = t3)
+            //println("Got Fun, new body is " + funAltBody.body)
+            funAltBody
+        }
+        case t: App => {
+            //println("Got App, v is " + t.v)
+            t // no mod
+        }
+    }
+    
+    /* 
     list foreach { _ match {
         case t: Var => {
             val varAltName = t.copy(name = t.name + "Alt")
             println("Got Var, new name is " + varAltName.name)
         }
-        case t: Fun => println("Got Fun")
-        case t: App => println("Got App")
-        case t: Term => println("Got Term")
+        case t: Fun => {
+            val funAltBody = t.copy(body = t3)
+            println("Got Fun, new body is " + funAltBody.body)
+        }
+        case t: App => {
+            println("Got App, v is " + t.v)
+        }
     }}
-         
+    */
 }
