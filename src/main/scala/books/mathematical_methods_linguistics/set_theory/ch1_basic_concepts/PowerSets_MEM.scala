0322d63472f8d8a652a0787f10254f5a0d0cb2f3
GitOutATown <aqumegeemail@gmail.com>
2015-12-29 15:13:05 -0800

    backup

--- src/main/scala/books/mathematical_methods_linguistics/set_theory/ch1_basic_concepts/PowerSets_MEM.scala
+++ src/main/scala/books/mathematical_methods_linguistics/set_theory/ch1_basic_concepts/PowerSets_MEM.scala
@@ -3,6 +3,7 @@ package books.mathematical_methods_linguistics.set_theory.ch1_basic_concepts
 object PowerSets_MEM {
 
     def powerMEM[A](s: Set[A]): Set[Set[A]] = {
+        @annotation.tailrec
         def inner(s: Set[A], ps: Set[Set[A]]): Set[Set[A]] = {
             if(s.isEmpty) ps
             else inner(s.tail, (ps ++ ( ps.map(_ + s.head) )))
